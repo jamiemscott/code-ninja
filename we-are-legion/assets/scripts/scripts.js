409476a788a460cd10c07c6637bbb3bde089b862
Jamie Scott <44259355+jamiemscott@users.noreply.github.com>
2025-01-13 16:18:58 +0000

    adding a redirects file

--- we-are-legion/assets/scripts/scripts.js
+++ we-are-legion/assets/scripts/scripts.js
@@ -1,6 +1,11 @@
-function initMap() {
-  const mapOptions = {}
-
-  const map = new google.maps.Map(document.getElementById("legionMap"), mapOptions);
-
+function initMap() 
+{
+   const centerMap = {} // We'll add in some options shortly
+		
+   const mapOptions = {
+      center: centerMap, // coordinates to center map
+      zoom: 10, // level of zoom we want
+   }
+		
+   const map = new google.maps.Map(document.getElementById("legionMap"), mapOptions);	
 }
